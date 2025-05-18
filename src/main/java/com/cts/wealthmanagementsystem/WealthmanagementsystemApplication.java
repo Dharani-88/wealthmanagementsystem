cb701f0ec02988fcf5818ef02a8a8aed1c7686aa
Dharani466 <2401267@LTIN620378.cts.com>
2025-05-18 21:27:36 +0530

    Skeleton commit

---
+++ src/main/java/com/cts/wealthmanagementsystem/WealthmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.cts.wealthmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cts/wealthmanagementsystem/WealthmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.wealthmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
